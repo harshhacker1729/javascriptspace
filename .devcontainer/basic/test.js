ce687211c9e08b9211e534cff0ca1db761a2d462
harshhacker1729 <165643619+harshhacker1729@users.noreply.github.com>
2024-10-09 12:23:39 +0000

    console

--- .devcontainer/basic/test.js
+++ .devcontainer/basic/test.js
@@ -1,1 +1,11 @@
-console.log("hello")
+console.log("hello")  
+let age =18
+let isLoggedIn = false
+//number => 2 to power 53
+// bigint =
+// string  => ""
+// boolen => true / false 
+// null => standalone value
+// undefined => 
+// symbol => 
+console.log(typeof null)
