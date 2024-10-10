f041f98decfa0cb28322726e77ac791ee540292f
harshhacker1729 <165643619+harshhacker1729@users.noreply.github.com>
2024-10-10 05:58:27 +0000

    10oct

---
+++ .devcontainer/basic/today.js
@@ -0,0 +1,9 @@
+let harsh = 3
+let om =4n
+harsh++
+++om
+// console.log(harsh)
+// console.log(om)
+// console.log("2" === 2) // but data types check karta hai 
+// console.log("2"==2) // data conversion nhi dekhata hai 
+console.log(`hello my age is ${harsh} and friend age is ${om}`)
