52d39cf9cfe3265af28a6ada1177d338c250d9a1
harshhacker1729 <165643619+harshhacker1729@users.noreply.github.com>
2024-10-11 06:38:27 +0000

    10oct

---
+++ .devcontainer/basic/10oct.js
@@ -0,0 +1,9 @@
+const harsh  = 200
+console.log(harsh)
+const om = new Number(100000.499)
+console.log(om)
+const abhi =new String(200)
+console.log(abhi)
+console.log(om.toFixed(3)) //toFixed used to give float value
+console.log(om.toPrecision(2)) //toPrecision used to give roundoff
+console.log(om.toLocaleString('en-IN')) //to give comma in number 
--- .devcontainer/basic/today.js
+++ .devcontainer/basic/today.js
@@ -7,3 +7,11 @@ harsh++
 // console.log("2" === 2) // but data types check karta hai 
 // console.log("2"==2) // data conversion nhi dekhata hai 
 console.log(`hello my age is ${harsh} and friend age is ${om}`)
+let listing = "harsh"
+console.log(listing[3]) // negative doesnot work here and out of index give error undefined
+console.log(listing.length)
+console.log(listing.toUpperCase())
+console.log(listing.charAt(2))
+console.log(listing.indexOf("h"))
+console.log(listing.substring(1,4))
+//includes replace
